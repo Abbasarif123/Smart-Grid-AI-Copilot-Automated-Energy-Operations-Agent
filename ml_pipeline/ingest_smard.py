ee0d54b361f527c84f6924f0876dca0f8a9b5bfa
Syed Muhammad Abbas Arif <syar00001@stud.uni-saarland.de>
2026-08-20 12:48:26 +0000

    first file

---
+++ ml_pipeline/ingest_smard.py
@@ -0,0 +1,66 @@
+import requests
+import pandas as pd
+from datetime import datetime
+import json
+from pathlib import Path
+
+#we need to: get the SMARD data feed for day ahead electricty spot market prices
+#transform raw JSON to time series data
+#export into parquet file
+
+
+def fetch_smard_data(filter_id: int, region_id: str, resolution: str, timestamp: int) -> pd.DataFrame:
+    """
+    Fetches time-series data from the SMARD API
+    
+    Args:
+        filter_id: 4169 for Spot Market Price, 1223 for Actual Generation
+        region_id: 'DE' for Germany
+        resolution: 'hour' or 'quarterhour'
+        timestamp: Unix timestamp in milliseconds for the week to fetch
+    """
+    #construct the specific API endpoint URL dynamically using the provided fucntion arguments
+    url = f"https://www.smard.de/app/chart_data/{filter_id}/{region_id}/{filter_id}_{region_id}_{resolution}_{timestamp}.json"
+    
+    response = requests.get(url)
+    #check if successful
+    response.raise_for_status()
+    
+    data = response.json()["series"]
+    
+    # convert to DataFrame
+    df = pd.DataFrame(data, columns=["timestamp", "value"])
+    
+    # convert timestamps from milliseconds to datetime
+    df["timestamp"] = pd.to_datetime(df["timestamp"], unit="ms")
+    df.set_index("timestamp", inplace=True)
+    
+    return df
+
+if __name__ == "__main__":
+    print("Fetching German electricity spot prices...")
+    
+    # example: A specific week timestamp required by the API (e.g recent Monday)
+    # SMARD requires timestamps aligned to specific weeks ie: 1709506800000 is approx early March 2024.
+    sample_timestamp = 1709506800000 
+    
+    try:
+        # fetch the day ahead spot price
+        df_prices = fetch_smard_data(
+            filter_id=4169, 
+            region_id="DE", 
+            resolution="hour", 
+            timestamp=sample_timestamp
+        )
+        
+        print(df_prices.head())
+        
+        # save to the data directory as Parquet (efficient storage)
+        output_dir = Path("../data")
+        output_dir.mkdir(exist_ok=True)
+        df_prices.to_parquet(output_dir / "spot_prices.parquet")
+        
+        print(f"Data saved successfully to {output_dir.resolve()}/spot_prices.parquet")
+        
+    except requests.exceptions.HTTPError as e:
+        print(f"Failed to fetch data: {e}")
